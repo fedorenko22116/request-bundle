b7053d44d4706dd1a0daf349af136c84b086bcb4
victor.fedorenko <victor.fedorenko@mgid.com>
2020-03-28 22:58:44 +0200

    Corrected E2e tests

--- tests/E2e/src/Request/JsonRpcRequest.php
+++ tests/E2e/src/Request/JsonRpcRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Request;
 
+use LSBProject\RequestBundle\Configuration\PropConverter;
 use LSBProject\RequestBundle\Configuration\RequestStorage;
 use LSBProject\RequestBundle\Request\AbstractRequest;
 
@@ -11,6 +12,12 @@ use LSBProject\RequestBundle\Request\AbstractRequest;
 abstract class JsonRpcRequest extends AbstractRequest
 {
     public string $jsonrpc;
-    public string $method;
     public int $id;
+
+    /**
+     * 'method' property already present in a base Request class, so alias should be used
+     *
+     * @PropConverter(name="method")
+     */
+    public string $methodName;
 }
